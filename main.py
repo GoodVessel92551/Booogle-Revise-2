c0e07a4f8276b64fe9fd484a513469bb1668d773
GoodVessel92551 <100193053+GoodVessel92551@users.noreply.github.com>
2023-02-08 17:07:39 +0000

    BETA

--- main.py
+++ main.py
@@ -3,11 +3,56 @@ from replit import web
 from fun import user,follow,make_dict,pro_pic
 app = Flask(__name__)
 
+test_app = {
+    "set_name":{
+        "settings":{
+            "name":"name",
+            "desc":"desc",
+            "public":"true",
+            "background":"maths",
+            "user":"username",
+        },
+        "Q1":{
+            "question":"question",
+            "answers":{
+                "ans1":"ans1",
+            }
+        },
+        "Q2":{
+            "question":"question",
+            "answers":{
+                "ans1":"ans1",
+            },
+        },
+    },
+    "set_name_1":{
+        "settings":{
+            "name":"name",
+            "desc":"desc",
+            "public":"true",
+            "background":"maths",
+            "user":"username",
+        },
+        "Q1":{
+            "question":"question",
+            "answers":{
+                "ans1":"ans1",
+            }
+        },
+        "Q2":{
+            "question":"question",
+            "answers":{
+                "ans1":"ans1",
+            }
+        }
+    }
+}
+
 
 @app.route("/")
 @web.authenticated_template("login.html")
 def home():
-    return render_template("home.html",name=user(),boosting=follow(user()),profile_pic=pro_pic())
+    return render_template("home.html",name=user(),boosting=follow(user()),profile_pic=pro_pic(),sets=test_app)
 
 @app.route("/new")
 @web.authenticated_template("login.html")
@@ -31,5 +76,10 @@ def sw():
 @app.route("/offline")
 def offline():
     return render_template("offline.html")
+
+@app.route("/flashcards")
+@web.authenticated_template("login.html")
+def flashcards():
+    return render_template("cards.html",name=user(),boosting=follow(user()),profile_pic=pro_pic())
     
 app.run(host="0.0.0.0",port=8080,debug=True)
